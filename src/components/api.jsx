001cc2c40a149c9f2c3ebf26ecca40b07142177a
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-30 13:46:10 +0530

    Update api.jsx

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080/api/stocks";
+const API_URL = "joyful-consideration-production.up.railway.app/api/stocks";
 
 export const fetchStocks = () => axios.get(API_URL);
 export const addStock = (stock) => axios.post(API_URL, stock);
